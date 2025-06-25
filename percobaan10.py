e62c1fe171eb3941896b17566b06aa0b2ed1ef20
NisrinaZulfa <pmlnisrina@gmail.com>
2025-06-25 23:50:33 +0700

    Delete percobaan10.py

--- percobaan10.py
+++
@@ -1,9 +0,0 @@
-# Praktikum Python Minggu 10
-# Materi: Visualisasi dengan matplotlib
-import matplotlib.pyplot as plt
-
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.title('Contoh Plot')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.show()
